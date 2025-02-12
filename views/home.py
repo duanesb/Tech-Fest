d1adb54b892489174a679e261dd728fbc7227810
duanesb <duanebollig.school@gmail.com>
2025-02-12 19:25:53 -0400

    finally starting ui
    .

--- views/home.py
+++ views/home.py
@@ -5,8 +5,11 @@ def HomeContent():
     content = ft.Container(padding=0,width=appWidth,height=appHeight,
         content=ft.Column(
             controls=[
-                
-            ]
-        )
+                ft.Text("Student ID Card Management",size=40,color="black",weight="bold",
+                        width=appWidth/1.5,text_align=ft.TextAlign.CENTER)
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        ),
+        margin=ft.margin.only(top=20)
     )
     return content
