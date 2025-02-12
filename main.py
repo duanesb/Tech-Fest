2c4fb08ab70e93241b2c1a5c99f408b0fdfac95d
duanesb <duanebollig.school@gmail.com>
2025-02-12 17:41:25 -0400

    cooking

--- main.py
+++ main.py
@@ -4,3 +4,25 @@ class View(ft.View):
     def __init__(self,route,content):
         super().__init__(route=route)
         self.controls.append(content)
+    def set(self,page):
+        page.views.append(self)
+
+def main(page: ft.Page):
+    # CONFIG
+    page.title = "Student ID Card"
+    page.window.width = 500
+    page.window.height = 500
+
+    # ROUTING
+    def route_change(route):
+        page.views.clear()
+        match(page.route):
+            case _:
+                pass
+    
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view)
+
+ft.app(main)
---
+++ views/home.py
@@ -0,0 +1,5 @@
+import flet as ft
+
+def Home():
+    content = ft.Container()
+    return content
