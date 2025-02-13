779b5001c1712b40ea162d0dca463200dfa03946
duanesb <duanebollig.school@gmail.com>
2025-02-12 23:41:49 -0400

    work on register ui

--- objects.py
+++ objects.py
@@ -21,9 +21,19 @@ class ElevatedButton(ft.ElevatedButton):
         self.bgcolor = "#353535"
         self.color = "white"
         self.width = appWidth*0.35
-        self.height = 60
+        self.height = 50
         self.style = ft.ButtonStyle(
             shape={"":ft.RoundedRectangleBorder(radius=5)},
-            side=ft.BorderSide(width=5,color="#171717"),
+            side=ft.BorderSide(width=2,color="#171717"),
             text_style=ft.TextStyle(size=15,weight=ft.FontWeight.BOLD))
         self.on_click=function
+
+class TextField(ft.TextField):
+    def __init__(self,label):
+        super().__init__()
+        self.label = label
+        self.width = appWidth*0.7
+        self.height = 50
+        self.border_width = 2
+        self.border_color = "#878787"
+        self.color = "black"
--- views/register.py
+++ views/register.py
@@ -1,18 +1,28 @@
 import flet as ft
-from objects import appWidth,appHeight,ElevatedButton
+from objects import appWidth,appHeight,ElevatedButton,TextField
 
 def RegisterContent():
     content = ft.Container(width=appWidth,height=appHeight,
         content=ft.Column(
             controls=[
+                ft.Text("Register Student",size=35,color="black",weight="bold",
+                        width=appWidth/1.5,text_align=ft.TextAlign.CENTER),
+                TextField("Name"),
+                TextField("Student ID"),
                 ft.Row(
                     controls=[
                         ElevatedButton("Return Home",lambda _:_.page.go("/home")),
                         ElevatedButton("Register",None),
                     ],
                     alignment=ft.MainAxisAlignment.CENTER
-                )
-            ]
+                ),
+                ft.Stack([
+                    ft.Image(src="idCardEmpty.png",width=appWidth*0.8),
+                    ft.Text()
+                ])
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=20
         )
     )
     return content
