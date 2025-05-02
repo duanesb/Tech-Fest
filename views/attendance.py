e2d1b737a407c403f4b8470912ef15d53650ef96
duanesb <duanebollig.school@gmail.com>
2025-05-02 00:11:18 -0400

    starting attendance

--- views/attendance.py
+++ views/attendance.py
@@ -1,13 +1,15 @@
 import flet as ft
 from objects import appWidth,appHeight,ElevatedButton,TextField
 import sqlite3
+from datetime import datetime, timedelta
+
 
 def AttendanceContent():
     content = ft.Container(width=appWidth,height=appHeight,
         content= ft.Column(
             controls=[
-                ft.Text("attendance")
-            ]
+                ft.Text(f"Attendance {datetime.now().strftime("%Y-%m-%d")}",color="black",size=30,weight=ft.FontWeight.BOLD, width=appWidth, text_align=ft.TextAlign.CENTER)
+            ],
         )
     )
     return content
